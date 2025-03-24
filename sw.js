98e4750ebb0d2141cd3945ddd432ba3329f34583
DimeNiceTry <chdd04@gmail.com>
2025-03-24 12:41:10 +0300

    added pwa

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'todo-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/src/app.js',
+  '/src/storage.js',
+  '/src/ui.js',
+  '/src/theme.js',
+  '/src/analytics.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then(response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          });
+      })
+  );
+}); 
